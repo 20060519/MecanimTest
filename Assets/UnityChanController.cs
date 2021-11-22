89c6d6aa6abef0fe24e4feab3d2802d713144fd9
20060519 <91940216+20060519@users.noreply.github.com>
2021-11-22 19:12:07 +0900

    Initial commit

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスがクリックされたとき
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Animatorコンポーネントを取得し、"jumpTrigger"をtrueにする
+            GetComponent<Animator>().SetTrigger("jumpController");
+        }
+        
+    }
+}
